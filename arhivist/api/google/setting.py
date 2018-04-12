75d5b1bf55c3bda4e76843a0a82ee113a9d530d1
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-04-12 09:19:54 +0300

    move secret dev api key to protected user folder.

--- arhivist/api/google/setting.py
+++ arhivist/api/google/setting.py
@@ -1,2 +1,13 @@
 # coding: utf-8
-API_KEY = 'AIzaSyCmFU5yTR83R4BQbCk4a1OocFy6Hxy2qxc'
+import os
+
+# Google API keys
+HOME_DIR = os.environ["HOME"]
+API_DIR = os.path.join(HOME_DIR, ".google")
+API_KEY_FILE_NAME = "api_key"
+API_KEY_PATH = os.path.join(API_DIR, API_KEY_FILE_NAME)
+
+if not os.path.exists(API_KEY_PATH):
+    raise IOError("Api key doesn't exist")
+
+API_KEY = "".join(line for line in open(API_KEY_PATH)).strip()
